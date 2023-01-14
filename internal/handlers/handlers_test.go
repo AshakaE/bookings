006558d8c0b7c8bac520b150e1e4179d2ec19cc5
AshakaE <ashakaegerega@gmail.com>
2023-01-14 15:54:55 +0100

    Test Post handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -25,9 +26,23 @@ var theTests = []struct {
 	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"post-search-availability", "/search-availability", "POST", []postData{
+		{key: "start", value: "2023-01-14"},
+		{key: "end", value: "2023-01-28"},
+	}, http.StatusOK},
+	{"post-search-availability-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2023-01-14"},
+		{key: "end", value: "2023-01-28"},
+	}, http.StatusOK},
+	{"mr", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "Ashaka"},
+		{key: "last_name", value: "Egerega"},
+		{key: "email", value: "ashaka@email.com"},
+		{key: "phone", value: "777-777-7777"},
+	}, http.StatusOK},
 }
 
-func TestHandlers(t * testing.T) {
+func TestHandlers(t *testing.T) {
 	routes := getRoutes()
 	ts := httptest.NewTLSServer(routes)
 	defer ts.Close()
@@ -44,7 +59,19 @@ func TestHandlers(t * testing.T) {
 				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 			}
 		} else {
-			
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
